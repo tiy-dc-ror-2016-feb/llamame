2d7d69e3598b1a12088fbfa484f2c8bb6574becc
Nadia Barbosa <nadiabarbosa@me.com>
2016-04-26 16:25:27 -0400

    Add test for dashboard#index

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can get dashboard index" do
+    get :index
+
+    assert_response :success
+  end
 end
