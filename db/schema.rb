369982fe0187cc00e6d216c2ce7dc5b08d49951f
samk1013 <samk1013@gmail.com>
2016-04-26 15:52:06 -0400

    Fix gemfile and remove bcrypt from user fixture

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426184326) do
+ActiveRecord::Schema.define(version: 20160426192712) do
 
   create_table "interactions", force: :cascade do |t|
     t.string   "topic"
