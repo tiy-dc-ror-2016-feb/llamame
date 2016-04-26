11a680355b8be260a424990369c088bea0025979
Mike West <westmd23@gmail.com>
2016-04-26 15:10:34 -0400

    attempting to merge locally

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,14 +1,18 @@
 class CompaniesController < ApplicationController
   def index
+    @companies = Company.all
   end
 
   def new
+    @company = Company.new
   end
 
   def show
+    @company = Company.find(params[:id])
   end
 
   def edit
+    @company = Company.find(params[:id])
   end
 
   def create
