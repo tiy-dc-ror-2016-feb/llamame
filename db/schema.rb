160df0ad6c62c7ee173fb7ae09d7b1e01d641d3f
Bo Butcher <cbbutcher91@gmail.com>
2016-04-26 16:42:59 -0400

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426173356) do
+ActiveRecord::Schema.define(version: 20160426185252) do
+
+  create_table "notes", force: :cascade do |t|
+    t.integer  "noteable_id"
+    t.string   "noteable_type"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "notes", ["user_id"], name: "index_notes_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
