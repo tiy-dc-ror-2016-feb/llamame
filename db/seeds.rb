455559034cfdc1cf40a10f0e4e30449341990340
Steve Fake <steven.m.fake@gmail.com>
2016-04-27 13:50:47 -0400

    Populate seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,20 @@
 end
 
 @boss = User.create(email: "boss@boss.com", password: "password", first_name: "Sam", last_name: "Kim", is_a_boss: true)
+
+20.times do
+  User.create(email: FFaker::Internet.email, password: "password", first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name)
+end
+
+25.times do
+  Company.create(name: FFaker::Company.name, salesperson: FFaker::Name.name, notes: FFaker::Lorem.sentences)
+end
+
+15.times do
+  Note.create(body: FFaker::Lorem.paragraphs)
+end
+
+30.times do |list|
+  list = ["email", "phone", "voicemail", "in person"]
+  Interaction.create(topic: FFaker::Lorem.sentence, medium: list[rand(0..3)], details: FFaker::Lorem.paragraphs, follow_up_date: FFaker::Time.date)
+end
