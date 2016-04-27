e71392a7945ab30e917d017f8ded83361d945721
Nadia Barbosa <nadiabarbosa@me.com>
2016-04-27 14:25:13 -0400

    Add medium types for interactions

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -1,2 +1,5 @@
 class Interaction < ActiveRecord::Base
+  MEDIUMS = { email: "Email", phone: "Phone", in_person: "In Person" }
+
+  validates :medium, inclusion: { in: MEDIUMS.keys.map(&:to_s) }
 end
