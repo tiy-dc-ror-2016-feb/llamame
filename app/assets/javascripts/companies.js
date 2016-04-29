0ba781021a3e0000de47eaa4386ad39a633ba424
Nadia Barbosa <nadiabarbosa@me.com>
2016-04-29 10:10:25 -0400

    Display map with no points on companies#show

---
+++ app/assets/javascripts/companies.js
@@ -0,0 +1,14 @@
+console.log('I work on the companies page')
+
+
+$( document ).ready(function() {
+  mapboxgl.accessToken = 'pk.eyJ1IjoibmJiMTI4MDUiLCJhIjoiMUlFVjZWVSJ9.jeNyiaRq8MCHlXSlGQZIHA';
+  var map = new mapboxgl.Map({
+      container: 'map', // container id
+      style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
+      center: [-74.50, 40], // starting position
+      zoom: 9 // starting zoom
+  });
+});
+
+console.log('END')
