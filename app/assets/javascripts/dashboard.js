789dc5e6e639ac61a6a758db6f168ccda4b39b44
Nadia Barbosa <nadiabarbosa@me.com>
2016-04-30 14:51:50 -0400

    Improved map

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,25 +1,90 @@
 $( document ).ready(function() {
 
-  $( "#followups-needed-btn" ).click(function() {
-    $( "#followups-needed-panel" ).slideToggle( function() {
-      $( "#recent-interactions-panel" ).hide();
-    });
-  });
+  $("#followups-needed-btn").click(function() {
+    $("#followups-needed-panel").slideToggle(function() {
+      $("#recent-interactions-panel").hide();
+       });
+   });
 
-  $( "#recent-interactions-btn" ).click(function() {
-    $( "#recent-interactions-panel" ).slideToggle( function() {
-      $( "#followups-needed-panel" ).hide();
-    });
+  $("#recent-interactions-btn").click(function () {
+    $("#recent-interactions-panel").slideToggle(function () {
+      $("#followups-needed-panel").hide();
+       });
   });
 
 });
 
-$(document.getElementById("#map")).ready(function() {
-  mapboxgl.accessToken = 'pk.eyJ1IjoibmJiMTI4MDUiLCJhIjoiMUlFVjZWVSJ9.jeNyiaRq8MCHlXSlGQZIHA';
-  var map = new mapboxgl.Map({
-      container: 'map', // container id
-      style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
-      center: [-74.50, 40], // starting position
-      zoom: 9 // starting zoom
+/////////////////////   MAP JS   /////////////////////
+
+$(document.getElementById("#map")).ready(function () {
+  // Generate map
+    mapboxgl.accessToken = 'pk.eyJ1IjoibmJiMTI4MDUiLCJhIjoiMUlFVjZWVSJ9.jeNyiaRq8MCHlXSlGQZIHA';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v8',
+        center: [-124.0674054, 46.2899054],
+        zoom: 7
+      }
+    );
+
+    // Get company ID from companies#show URL
+    var urlArray = window.location.href.split("/");
+    var companyId = urlArray.pop();
+
+    // Return address Json of that company
+    var companyJson = $.ajax({
+             url: "/companies/"+companyId,
+             type: "GET",
+             dataType: "json",
+             success: function(data){
+               var address = data.companies[0];
+             }
+    });
+
+    map.on('load', function () {
+      map.addSource("markers", {
+          "type": "geojson",
+          "data": {
+              "type": "FeatureCollection",
+              "features": [{
+                  "type": "Feature",
+                  "geometry": {
+                      "type": "Point",
+                      "coordinates": [-77.03238901390978, 38.913188059745586]
+                  },
+                  "properties": {
+                      "title": "Mapbox DC",
+                      "marker-symbol": "monument"
+                  }
+              }, {
+                  "type": "Feature",
+                  "geometry": {
+                      "type": "Point",
+                      "coordinates": [-122.414, 37.776]
+                  },
+                  "properties": {
+                      "title": "Mapbox SF",
+                      "marker-symbol": "harbor"
+                  }
+              }]
+          }
+      });
+
+      map.addLayer({
+          "id": "markers",
+          "type": "symbol",
+          "source": "markers",
+          "layout": {
+              "icon-image": "{marker-symbol}-15",
+              "text-field": "{title}",
+              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+              "text-offset": [0, 0.6],
+              "text-anchor": "top"
+          }
+      });
   });
 });
+
+
+
+/////////////////////   MAP JS   /////////////////////
--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -16,9 +16,11 @@ class CompaniesController < ApplicationController
   # GET /companies/1
   # GET /companies/1.json
   def show
+    @company = Company.find(params[:id])
+
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @company }
+      format.json { render json: @company.addresses }
     end
   end
 
