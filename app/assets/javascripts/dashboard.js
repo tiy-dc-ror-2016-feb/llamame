02bbf898e42487ade0720e836724e5cf653f6129
Nadia Barbosa <nadiabarbosa@me.com>
2016-04-30 16:09:48 -0400

    Map updates

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -22,8 +22,8 @@ $(document.getElementById("#map")).ready(function () {
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v8',
-        center: [-124.0674054, 46.2899054],
-        zoom: 7
+        center: [-98.583333, 39.833333],
+        zoom: 3
       }
     );
 
@@ -38,53 +38,58 @@ $(document.getElementById("#map")).ready(function () {
              dataType: "json",
              success: function(data){
                var address = data.companies[0];
+               var street = data.companies[0].address_1;
+               var city = data.companies[0].city;
+               var state = data.companies[0].state;
+               var zip_code = data.companies[0].zip_code;
+               var full_address = street+" "+city+" "+state+" "+zip_code;
+               var encoded_address = encodeURI(full_address);
+
+               console.log(full_address);
+               console.log(encoded_address);
+
+               // Convert to a lat/long coordinate
+               // Not hitting API correctly here
+               $.get( "https://api.mapbox.com/geocoding/v5/mapbox.places/"+encoded_address+".json?&access_token=pk.eyJ1IjoibmJiMTI4MDUiLCJhIjoiMUlFVjZWVSJ9.jeNyiaRq8MCHlXSlGQZIHA", function( data ) {
+                  console.log(data);
+                });
+
+               // Add point to map as a geojson
+               map.on('load', function () {
+                 map.addSource("markers", {
+                     "type": "geojson",
+                     "data": {
+                         "type": "FeatureCollection",
+                         "features": [{
+                             "type": "Feature",
+                             "geometry": {
+                                 "type": "Point",
+                                 "coordinates": [-77.03238901390978, 38.913188059745586]
+                             },
+                             "properties": {
+                                 "title": "You Are Here!",
+                                 "marker-symbol": "marker"
+                             }
+                         }]
+                     }
+                 });
+
+                 map.addLayer({
+                     "id": "markers",
+                     "type": "symbol",
+                     "source": "markers",
+                     "layout": {
+                         "icon-image": "{marker-symbol}-15",
+                         "text-field": "{title}",
+                         "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                         "text-offset": [0, 0.6],
+                         "text-anchor": "top"
+                       }
+                 });
+               });
              }
-    });
-
-    map.on('load', function () {
-      map.addSource("markers", {
-          "type": "geojson",
-          "data": {
-              "type": "FeatureCollection",
-              "features": [{
-                  "type": "Feature",
-                  "geometry": {
-                      "type": "Point",
-                      "coordinates": [-77.03238901390978, 38.913188059745586]
-                  },
-                  "properties": {
-                      "title": "Mapbox DC",
-                      "marker-symbol": "monument"
-                  }
-              }, {
-                  "type": "Feature",
-                  "geometry": {
-                      "type": "Point",
-                      "coordinates": [-122.414, 37.776]
-                  },
-                  "properties": {
-                      "title": "Mapbox SF",
-                      "marker-symbol": "harbor"
-                  }
-              }]
-          }
-      });
-
-      map.addLayer({
-          "id": "markers",
-          "type": "symbol",
-          "source": "markers",
-          "layout": {
-              "icon-image": "{marker-symbol}-15",
-              "text-field": "{title}",
-              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-              "text-offset": [0, 0.6],
-              "text-anchor": "top"
-          }
-      });
-  });
-});
-
+           });
+         });
 
 
 /////////////////////   MAP JS   /////////////////////
