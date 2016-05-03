2ac79785fa56b910c96f2913b49fa769a7fb0bbb
Russell Osborne <russell@burningpony.com>
2016-05-02 20:46:11 -0400

    Fix capybara test suite, also fixes stub login.

--- test/integration/interaction_form_test.rb
+++ test/integration/interaction_form_test.rb
@@ -2,10 +2,7 @@ require 'test_helper'
 
 class InteractionFormTest < ActionDispatch::IntegrationTest
   setup do
-    visit new_user_session_path
-    fill_in("Email", with: "sam@sam.com")
-    fill_in("Password", with: "passw0rd!")
-    click_button("Log in")
+    login_as users(:user_1), scope: :user
   end
 
   focus
@@ -20,13 +17,14 @@ class InteractionFormTest < ActionDispatch::IntegrationTest
 
     click_link("Add Interaction")
 
-    fill_in("Topic", :with => "Llama Production Forecasts")
-    select("Phone", :from => "Medium")
-    fill_in("Details", :with => "We talked about the 5 year forecast.")
-    fill_in("Follow up date", :with => "01/01/2001")
+    fill_in("Topic", with: "Llama Production Forecasts")
+    select("Phone", from: "Medium")
+    fill_in("Details", with: "We talked about the 5 year forecast.")
+    fill_in("Follow up date", with: "01/01/2001")
     click_button("Create Interaction")
 
-    sleep(5)
-    binding.pry
+    within ".flash-messages" do
+      assert page.has_content?("Success")
+    end
   end
 end
